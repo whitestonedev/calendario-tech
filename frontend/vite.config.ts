e4d6965040e3d20fb43bc617a686a8f60550d48c
VStahelin <vitor@stahelin.dev>
2025-02-14 23:17:31 -0300

    fix: update homepage URL and adjust Vite base configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/calendario-istepo/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/calendario-istepo/' : '/',
   plugins: [
     react(),
     VitePWA({
