82757e7475fbbd915afdf06fd90cb23d3c08b1cc
Vitor José Stähelin <vitorjosestahelin@gmail.com>
2025-05-22 22:59:03 -0300

    feat: add endpoint to retrieve events to approve

--- backend/src/routes/events.py
+++ backend/src/routes/events.py
@@ -4,6 +4,7 @@ from flask import jsonify
 from flask_openapi3 import Tag, APIBlueprint
 
 from src.exceptions import DuplicateEventException, EventNotFoundException
+from src.models import EventStatus
 from src.schemas import (
     EventIn,
     EventQuery,
@@ -23,24 +24,44 @@ from src.services.event import (
 from src.services.event import update_event as update_event_service
 
 event_bp = APIBlueprint("events", __name__, url_prefix="/events")
-event_tag = Tag(name="events", description="Operations related to events")
+public_tag = Tag(
+    name="Public Events", description="Public endpoints for listing and viewing events."
+)
+submission_tag = Tag(
+    name="Event Submission", description="Endpoints for submitting new events."
+)
+review_tag = Tag(
+    name="Event Review (Staff)",
+    description="Endpoints for reviewing and managing submitted events (staff only).",
+)
+
 
 logger = logging.getLogger(__name__)
 
 
 @event_bp.get(
     "/",
-    tags=[event_tag],
+    tags=[public_tag],
     summary="Retrieve events",
 )
 def get_events(query: EventQuery):
-    events = get_events_service(query, only_approved=True)
+    events = get_events_service(query)
     return jsonify(events), 200
+
+
+@event_bp.get(
+    "/<int:event_id>",
+    tags=[public_tag],
+    summary="Retrieve events",
+)
+def get_event(path: EventPath):
+    event = get_event_service(path.event_id)
+    return jsonify(event.serialized), 200
 
 
 @event_bp.post(
     "/submit/",
-    tags=[event_tag],
+    tags=[submission_tag],
     summary="Submit event for review",
     security=[{"BearerAuth": []}],
 )
@@ -58,10 +79,63 @@ def create_event(body: EventIn):
     return jsonify(event.serialized), 201
 
 
+@event_bp.delete(
+    "/<int:event_id>",
+    tags=[review_tag],
+    summary="Delete event",
+    security=[{"BearerAuth": []}],
+)
+def delete_event(path: EventPath):
+    is_valid_credentials = check_credentials()
+    if is_valid_credentials:
+        return is_valid_credentials, 401
+
+    try:
+        delete_event_service(path.event_id)
+        return jsonify({"message": "Event deleted"}), 200
+    except EventNotFoundException as e:
+        return jsonify({"error": str(e)}), 404
+
+
+@event_bp.put(
+    "/<int:event_id>",
+    tags=[review_tag],
+    summary="Update event",
+)
+def update_event(event_id: int, event_data: EventUpdate):
+    is_valid_credentials = check_credentials()
+    if is_valid_credentials:
+        return is_valid_credentials, 401
+
+    event = update_event_service(event_id, event_data)
+
+    return jsonify(event.serialized)
+
+
+@event_bp.get(
+    "/submit/review/",
+    tags=[review_tag],
+    summary="Retrieve events pending review",
+    description="Fetches a list of events that are pending approval by the staff.",
+    security=[{"BearerAuth": []}],
+)
+def get_pending_events():
+    is_valid_credentials = check_credentials()
+    if is_valid_credentials:
+        return is_valid_credentials, 401
+
+    try:
+        events = get_events_service(status=EventStatus.requested)
+        return jsonify(events), 200
+    except Exception as e:
+        logger.error(f"Error fetching pending events: {e}")
+        return jsonify({"error": "Internal server error"}), 500
+
+
 @event_bp.post(
     "/submit/<int:event_id>",
-    tags=[event_tag],
-    summary="Manage submitted event (Staff Review)",
+    tags=[review_tag],
+    summary="Manage submitted event, approve or decline",
     description="Sets the status of an event to 'approved' or 'declined'.",
     security=[{"BearerAuth": []}],
 )
@@ -84,36 +158,3 @@ def manage_submitted_update_event(path: EventPath, body: ManageSubmittedEventBod
 
     except EventNotFoundException as e:
         return jsonify({"error": str(e)}), 404
-
-
-@event_bp.delete(
-    "/<int:event_id>",
-    tags=[event_tag],
-    summary="Delete event (Staff Action)",
-    security=[{"BearerAuth": []}],
-)
-def delete_event(path: EventPath):
-    is_valid_credentials = check_credentials()
-    if is_valid_credentials:
-        return is_valid_credentials, 401
-
-    try:
-        delete_event_service(path.event_id)
-        return jsonify({"message": "Event deleted"}), 200
-    except EventNotFoundException as e:
-        return jsonify({"error": str(e)}), 404
-
-
-@event_bp.put(
-    "/<int:event_id>",
-    tags=[event_tag],
-    summary="Update event (Staff Action)",
-)
-def update_event(event_id: int, event_data: EventUpdate):
-    is_valid_credentials = check_credentials()
-    if is_valid_credentials:
-        return is_valid_credentials, 401
-
-    event = update_event_service(event_id, event_data)
-
-    return jsonify(event.serialized)
